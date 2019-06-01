a57d4510329e1cc9b55065af7dc68edf0a48d835
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2019-06-01 17:42:59 -0300

    adcionar tabela mao de obra

--- projetoMetalGer/src/main/java/Model/MaoObraOrdemServico.java
+++ projetoMetalGer/src/main/java/Model/MaoObraOrdemServico.java
@@ -8,11 +8,13 @@ package Model;
 import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.GenericGenerator;
@@ -28,6 +30,8 @@ import org.hibernate.annotations.Parameter;
  *
  * @author walter heitor
  */
+@Entity
+@Table(name="mao_obra_servico")
 public class MaoObraOrdemServico {
     
 //variaveis
@@ -54,5 +58,76 @@ public class MaoObraOrdemServico {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, targetEntity = Insumo.class)
     @JoinColumn(name = "id_insumo", nullable = true)
     private Insumo insumo;
+    
+//contrutores
+
+    public MaoObraOrdemServico() {
+    }
+
+    public MaoObraOrdemServico(String descricaoOperacao, Date dt_fim, Date dt_inicio, float qtd_insumo, Pessoa pessoa, Insumo insumo) {
+        this.descricaoOperacao = descricaoOperacao;
+        this.dt_fim = dt_fim;
+        this.dt_inicio = dt_inicio;
+        this.qtd_insumo = qtd_insumo;
+        this.pessoa = pessoa;
+        this.insumo = insumo;
+    }
+
+    public Long getId_mao_obra() {
+        return id_mao_obra;
+    }
+
+    public void setId_mao_obra(Long id_mao_obra) {
+        this.id_mao_obra = id_mao_obra;
+    }
+
+    public String getDescricaoOperacao() {
+        return descricaoOperacao;
+    }
+
+    public void setDescricaoOperacao(String descricaoOperacao) {
+        this.descricaoOperacao = descricaoOperacao;
+    }
+
+    public Date getDt_fim() {
+        return dt_fim;
+    }
+
+    public void setDt_fim(Date dt_fim) {
+        this.dt_fim = dt_fim;
+    }
+
+    public Date getDt_inicio() {
+        return dt_inicio;
+    }
+
+    public void setDt_inicio(Date dt_inicio) {
+        this.dt_inicio = dt_inicio;
+    }
+
+    public float getQtd_insumo() {
+        return qtd_insumo;
+    }
+
+    public void setQtd_insumo(float qtd_insumo) {
+        this.qtd_insumo = qtd_insumo;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Insumo getInsumo() {
+        return insumo;
+    }
+
+    public void setInsumo(Insumo insumo) {
+        this.insumo = insumo;
+    }
+    
     
 }
