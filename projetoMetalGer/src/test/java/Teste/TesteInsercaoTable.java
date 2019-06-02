d4be76d27fa50e0f23a0223f9669bd0ebcbec177
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2019-06-01 23:03:25 -0300

    teste insercao2

--- projetoMetalGer/src/test/java/Teste/TesteInsercaoTable.java
+++ projetoMetalGer/src/test/java/Teste/TesteInsercaoTable.java
@@ -1,6 +1,7 @@
 package Teste;
 
 
+import Controller.FuncionarioDAO;
 import Controller.InsumoDAO;
 import Controller.ProjetosDAO;
 import Model.Cliente;
@@ -25,14 +26,18 @@ public class TesteInsercaoTable {
                 "cart_resrvis_categoria_func", "titulo_eleitor_func", "cart_identidade_func", new Date(90, 4, 3),
                 "orgao_emissorcart_identidade_func", "cpf_func", "pis_func", new Date(91, 7, 5), "local_nasc_func",
                 new Date(78, 5, 1), "estado_civil_func", "grau_instrucao_func", "peso_func", "cabelo_func", "pele_func", "defeitos_func", "nome", 1);
+      FuncionarioDAO.getInstance().persist(f);
+      System.out.println("----funcionario inserido com sucesso----");
       Cliente c = new Cliente("1234", "softWalter", "1234", "Walter", 2);  
       //ClienteDAO.getInstance().persist(c);
       Projetos p = new Projetos("itumbiara", "correia transprtadora", "lateral", "maria", "clara", "walter", "walter heitor", c);
       p.setPessoa(c);
       ProjetosDAO.getInstance().persist(p);
+      System.out.println("----cliente e projeto --- inserido com sucesso----");
      // ProjetosDAO.getInstance().merge(p);
       Insumo i = new Insumo("disco de corte", 123, 1);
       InsumoDAO.getInstance().persist(i);
+      System.out.println("----Insumo inserido com sucesso----");
         System.out.println("----------------teste realizado com sucesso------------------");
     }
     
